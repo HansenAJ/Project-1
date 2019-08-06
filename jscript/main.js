b2042b7f6dca14ae31559773a1a2f39f7fbc2c43
Alex Hansen <Hansen.Alexander.J@Gmail.com>
2019-08-06 15:16:37 -0400

    Started Psuedo Code

--- jscript/main.js
+++ jscript/main.js
@@ -1,1 +1,33 @@
 console.log("Hello World!")
+
+//Create array of Sci-Fi Movie Names.
+
+//Black Box One-time Run at App Load
+    //Use for-loop to generate on-screen keyboard and add listeners to each button.
+        //For Loop Length = Characters    
+            //Create Button A-Z
+            //Add Listener
+            //Append to DOM Element
+
+//Black Box "Add Boxes"
+    //Logic to randomly select Item from Array.
+    //Logic to seperate characters in selected item into their own Array.
+    //Logic to create Divs of Blank Boxes for each character in this new Array.
+                        //Invisible text in each box for tracking? Or secondary non-displayed box for tracking.
+        //For Loop Length = Characters
+            //Create Box 1-X
+            //Append to DOM Element
+
+//Black Box "New Game"
+    //Logic for New Game Button
+        //Call "Add Boxes"
+        //Add Six Space Marines
+        //Remove All Aliens
+        //Reset 'Round Score'
+        //Reset 'Win Score'
+
+//Logic for Reset Button
+    //Call "New Game"
+    //Reset Alien Score
+    //Reset Marine Score
+
